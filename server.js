bb99e56c70fad20885d6324bb56338c8bdb5db0a
ReaganS <reagansasan@gmail.com>
2022-07-29 17:46:42 +0200

    added multi file

--- server.js
+++ server.js
@@ -15,14 +15,44 @@ const fileStorage = multer.diskStorage({
     }
 })
 
-const upload = multer({ storage: fileStorage })
+const isPicture = (name, mimetype) => {
+    return(
+        name.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG)$/) && mimetype == "image/png" || mimetype == "image/jpg" || mimetype == "image/jpeg"
+    )
+}
+
+const fileFilter = (req, file, cb) => {
+    const { originalname, mimetype } = file;
+    if(!isPicture(originalname, mimetype)) {
+        req.fileValidationError = 'Please Pictures Only';
+        return cb('Please Pictures Only', false)
+    }
+    cb(null, true)
+}
+
+const upload = multer({ storage: fileStorage, fileFilter})
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.post('/upload-profile', upload.single('profile_pic') ,(req, res) => {
+
+    const { file, fileValidationError } = req;
+    
+    if (fileValidationError) {
+        return res.status(500).send(fileValidationError);
+      }
+    
+      if (!file) {
+        return res.status(400).send('Please upload a file');
+      }
     res.send(`<h2>Here is the picture:</h2><img src="http://localhost:5000/uploads/${req.file.filename}" alt="something" width="600"/>`)
+})
+
+app.post('/upload-cat-pics', upload.array('cat_pics', 10), (req, res, next) => {
+    console.log(req.files)
+    res.send(`<h2>Here are the pictures:</h2><img src="http://localhost:5000/uploads/${req.files.filename}" alt="something" width="600"/>`)
 })
 
 app.use('/uploads', express.static('uploads'));
@@ -33,6 +63,3 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-
-
-//WhatsApp Image 2022-04-01 at 9.08.39 AM.jpeg-1659086621355-338729442
